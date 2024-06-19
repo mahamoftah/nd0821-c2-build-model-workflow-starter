018dedf1f858e43c168c2b72adef99e57e31dedd
mahamoftah <mahamoftah19@gmail.com>
2024-06-19 16:31:31 +0000

    Project Building

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -5,6 +5,7 @@ Performs basic cleaning on the data and save the results in Weights & Biases
 import argparse
 import logging
 import wandb
+import pandas as pd
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
@@ -24,6 +25,23 @@ def go(args):
     # YOUR CODE HERE     #
     ######################
 
+    path = run.use_artifact(args.input_artifact).file()
+
+    df = pd.read_csv(path)
+    cleaned_df = df.dropna()
+
+    artifact = wandb.Artifact(
+        name="cleaned_df",
+        type="cleaned_df",
+        description="Data Preprocessing"
+    )
+
+    path_ = "cleaned_df.csv"
+    cleaned_df.to_csv(path_, index=False)
+    artifact.add_file(path_)
+
+    run.log_artifact(artifact)
+
 
 if __name__ == "__main__":
 
@@ -32,22 +50,22 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "--parameter1", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=int,                    ## INSERT TYPE HERE: str, float or int,
+        help="param 1",                   ## INSERT DESCRIPTION HERE,
         required=True
     )
 
     parser.add_argument(
         "--parameter2", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=int,                     ## INSERT TYPE HERE: str, float or int,
+        help="param2"                       ## INSERT DESCRIPTION HERE,
         required=True
     )
 
     parser.add_argument(
         "--parameter3", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=str,                   ## INSERT TYPE HERE: str, float or int,
+        help="param 3",                   ## INSERT DESCRIPTION HERE,
         required=True
     )
 
