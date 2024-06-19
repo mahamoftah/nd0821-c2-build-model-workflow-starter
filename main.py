b4f95ae5807b14a6c1dcacacfaf637eb3de3a0a4
mahamoftah <mahamoftah19@gmail.com>
2024-06-19 23:25:09 +0000

    Project Building

--- main.py
+++ main.py
@@ -52,30 +52,52 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-            # _ = mlflow.run(
-            #     f"sample.csv",
-            #     "main",
-            #     parameters={
-            #         "sample": config["etl"]["sample"],
-            #         "artifact_name": "cleaned_df.csv",
-            #         "artifact_type": "raw_data",
-            #         "artifact_description": "Cleaned_Data"
-            #     },
-            # )
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+                "main",
+                parameters={
+                    "input_artifact": "sample.csv:latest",
+                    "output_artifact": "clean_sample.csv",
+                    "output_type": "clean_sample",
+                    "output_description": "Data with outliers and null values removed",
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
+                },
+            )
             
 
         if "data_check" in active_steps:
             ##################
             # Implement here #
             ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+                "main",
+                parameters={
+                    "csv": "sample.csv:latest",
+                    "ref": "sample.csv:reference",
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price'],
+                },
+            )
 
         if "data_split" in active_steps:
             ##################
             # Implement here #
             ##################
-            pass
+
+             _ = mlflow.run(
+                  f"{config['main']['components_repository']}/train_val_test_split",
+                  "main",
+                  parameters={
+                      "input": "sample.csv:latest",
+                      "test_size": config['modeling']['test_size'],  
+                      "random_seed": config['modeling']['random_seed'], 
+                      "stratify_by": config['modeling']['stratify_by'],
+                  },
+              )
+
 
         if "train_random_forest" in active_steps:
 
